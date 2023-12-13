5cd5e20d41796083a30b1be9e35eebba8795f136
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2023-12-13 13:39:02 -0300

    feat: ai ai

---
+++ backend/prisma/migrations/20231213163600_/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "quantity" INTEGER NOT NULL
+);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,27 @@ type pizzaType = {
   price: number;
   ingredients: Array<string>;
 };
+type order = Pick<pizzaType, "name" | "price"> & { quantity: number };
 
 function App() {
   const [pizzas, setPizzas] = useState<pizzaType[] | null>();
-  const [orders, setOrders] = useState<string>();
+  const [ordersApi, setOrdersApi] = useState([]);
+  const [orders, setOrders] = useState<order>({
+    name: "",
+    price: 0,
+    quantity: 1,
+  });
+  const [refreshApi, setRefreshApi] = useState(false);
 
+  useEffect(() => {
+    const allOrders = async () => {
+      await fetch("http://localhost:3333/api/orders")
+        .then((result) => result.json())
+        .then((result) => setOrdersApi(result));
+      setRefreshApi(false);
+    };
+    allOrders();
+  }, [refreshApi]);
   useEffect(() => {
     const fetchApi = async () => {
       try {
@@ -25,9 +41,6 @@ function App() {
     fetchApi();
   }, []);
 
-  const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setOrders(event.target.value);
-  };
   const handleOrder = async () => {
     await fetch("http://localhost:3333/api/newOrder", {
       method: "POST",
@@ -36,6 +49,27 @@ function App() {
       },
       body: JSON.stringify(orders),
     });
+    setOrders({
+      name: "",
+      price: 0,
+      quantity: 1,
+    });
+    setRefreshApi(true);
+  };
+  const handleQuantityChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setOrders({
+      ...orders,
+      quantity: parseInt(event.target.value, 10),
+    });
+  };
+
+  const handleConfirm = (pizza: pizzaType) => {
+    const { name, price } = pizza;
+    setOrders({
+      ...orders,
+      name,
+      price: price ? orders?.quantity * price : 1,
+    });
   };
 
   return (
@@ -43,19 +77,12 @@ function App() {
       <div className="container">
         <div className="pizzas">
           <h1>Pizzas</h1>
-          {pizzas?.map((pizza) => (
-            <div
-              key={pizza.id}
-              className={`card ${orders === pizza.id ? "checked" : ""}`}
-            >
-              <input
-                type="radio"
-                id={pizza.name}
-                value={pizza.id}
-                checked={orders === pizza.id}
-                onChange={handleRadioChange}
-              />
-              <label htmlFor={pizza.name}>
+          <div className="container-pizzas">
+            {pizzas?.map((pizza) => (
+              <div
+                key={pizza.id}
+                className={`card ${orders.name === pizza.name ? "select" : ""}`}
+              >
                 <div className="card-header">{pizza.name}</div>
                 <div className="card-body">
                   <h3>R${pizza.price}</h3>
@@ -65,14 +92,41 @@ function App() {
                     ))}
                   </div>
                 </div>
-              </label>
-            </div>
-          ))}
-          <button onClick={handleOrder}>Confirme</button>
+                <div className="card-bottom">
+                  <label>
+                    Quantity:{" "}
+                    <input
+                      type="number"
+                      className="quantity"
+                      defaultValue={1}
+                      min={1}
+                      onChange={handleQuantityChange}
+                    />
+                  </label>
+                  <button className="add" onClick={() => handleConfirm(pizza)}>
+                    add
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <button className="add buy" onClick={handleOrder}>
+            Confirm
+          </button>
         </div>
-        {orders && (
+        {!!ordersApi[0] && (
           <div className="orders">
             <h1>Orders</h1>
+            <div className="container-pizzas">
+              {ordersApi?.map((pizza) => (
+                <div key={pizza.id} className="card">
+                  <div className="card-header">{pizza.name}</div>
+                  <div className="card-body">
+                    <h3>R${pizza.price}</h3>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         )}
       </div>
